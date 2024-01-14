357a59dc361288457c0d4cc2c988baad5f39dddf
Алакпаров Ибрагим Эхтият <Alakp_mr11@itstep.edu.az>
2024-01-14 10:41:11 +0400

    deleted old files and created new files

---
+++ signin.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const login = document.querySelector('.login');
+    const password = document.querySelector('.password');
+    const btn = document.querySelector('.btn');
+    const error = document.querySelector('.error-message');
+
+    btn.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        if (login.value === '' || password.value === '') {
+            error.textContent = 'Please enter both login and password.';
+        } else {
+            window.location.href = 'wheather/wheather.html';
+        }
+    });
+});
